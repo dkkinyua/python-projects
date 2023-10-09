1a71d2688ae3de65e66b8e7ed930a4e486583a87
Denzel Kanyeki <denzelkinyua11@gmail.com>
2023-10-10 01:16:32 +0300

    Update

--- Restaurant Reservation/restaurant_reservation.py
+++ Restaurant Reservation/restaurant_reservation.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import datetime as dt
 
-
-# This is a function that adds a reservation and exports it to a CSV file
 def add_reservation(reservation_list):
     first_name = input("Enter your first name: ")
     last_name = input("Enter your last name: ")
@@ -13,31 +11,31 @@ def add_reservation(reservation_list):
     restaurant_details = {
         "first_name": first_name,
         "last_name": last_name,
-        "guests expected": no_of_guests,
-        "date expected": date_expected,
-        "time expected": time_expected,
-        "time submitted": dt.datetime.now()
+        "guests_expected": no_of_guests,
+        "date_expected": date_expected,
+        "time_expected": time_expected,
+        "time_submitted": dt.datetime.now()
     }
-    
-    reservation_list.append(restaurant_details)  # Add the reservation dictionary to the list
 
-    # Create a DataFrame from the list of reservations
+    reservation_list.append(restaurant_details)
+
     df = pd.DataFrame(reservation_list)
-
-    # Export the DataFrame to a CSV file
     df.to_csv('reservations.csv', index=False)
 
     return reservation_list
 
-# This is a function that displays a reservation
 def display_reservation(reservation_dict):
-    print("Reservation details: ")
+    print("Reservation details:")
     for key, value in reservation_dict.items():
         print(f"{key}: {value}")
 
-# Initialize an empty list to store reservations
-reservation_list = []
-# Number of reservations
+# Load existing reservations from CSV if it exists
+try:
+    df = pd.read_csv('reservations.csv')
+    reservation_list = df.to_dict(orient='records')
+except FileNotFoundError:
+    reservation_list = []
+
 reservations = 10
 
 while reservations > 0:
@@ -46,12 +44,15 @@ while reservations > 0:
     print("1: Make a reservation")
     print("2: Display reservation")
     print("3: Exit program")
-    
+
     menu = input("Enter your choice (1/2/3): ")
 
     if menu == "1":
-        reservation_list = add_reservation(reservation_list)
-        reservations -= 1
+        if reservations > 0:
+            reservation_list = add_reservation(reservation_list)
+            reservations -= 1
+        else:
+            print("No more reservations available.")
     elif menu == "2":
         if len(reservation_list) > 0:
             for reservation in reservation_list:
@@ -59,13 +60,16 @@ while reservations > 0:
         else:
             print("No reservations have been made yet.")
     elif menu == "3":
-        break 
+        break
     else:
-        print("Invalid choice. Please select a valid option (1/2/3/4).")
+        print("Invalid choice. Please select a valid option (1/2/3).")
 
+# Save the final list of reservations to CSV
+df = pd.DataFrame(reservation_list)
+df.to_csv('reservations.csv', index=False)
 
+print("Exiting the program.")
 
-print("Number of reservations reached already.")
         
 
 
