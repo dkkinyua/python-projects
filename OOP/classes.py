a0065fe48282a9058450bfc4d0eb1215659bf91a
unknown <denzelkinyua11@gmail.com>
2024-04-24 03:33:22 +0300

    First Lesson: Classes and Instances in Python OOP

--- OOP/classes.py
+++ OOP/classes.py
@@ -9,8 +9,14 @@ class Employee:
         self.email = first + "." + last + "@company.co"
         self.pay = pay
 
-emp_1 = Employee("Dee", "Codes", 100000)
+    def fullname(self):
+        return f"{self.first} {self.last}"
+
+emp_1 = Employee("Dee", "Palmer", 100000)
 emp_2 = Employee("Lee", "King", 60000)
 
 print(emp_1.email)
 print(emp_2.email)
+
+print(emp_1.fullname())
+print(emp_2.fullname())
